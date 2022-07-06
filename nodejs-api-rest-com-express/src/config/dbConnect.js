9f2c53973ebc5def708970e76520c95bd91039c6
Samuel Gomes <51432896+Saesel@users.noreply.github.com>
2022-07-05 23:25:53 -0300

    Update dbConnect.js

--- nodejs-api-rest-com-express/src/config/dbConnect.js
+++ nodejs-api-rest-com-express/src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  "mongodb+srv://samuelgomes:NaquelePique26!@alura.4mtfl.mongodb.net/alura-node"
+  "mongodb+srv://@alura.4mtfl.mongodb.net/alura-node"
 );
 
 let db = mongoose.connection;
